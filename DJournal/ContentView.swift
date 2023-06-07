d1c81e02325029e51d8d6d6b08dc018cb4c6dfab
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-06-07 10:48:30 +0600

    Prepared basic UI for Journal

--- DJournal/ContentView.swift
+++ DJournal/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var messages: [String] = ["hello",
+                                     "This is me Rehnuma",
+                                     "This is an app where you can add your daily journal",
+                                     "you can post it here what you experience",
+                                     "Initially this only supports text based journal",
+                                     "Enjoy! :)"]
+
+    @State var text: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(messages, id: \.self) { message in
+                        Text(message)
+                    }
+                }
+                .background(.white)
+                
+                HStack(alignment: .bottom) {
+                    TextField("Write here", text: $text)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(minHeight: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .stroke(.blue, lineWidth: 1)
+                        }
+                        
+                    
+                    Button {
+                        //
+                    } label: {
+                        Image(systemName: "paperplane.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.blue)
+                    }
+                    
+                }
+                .frame(minWidth: 50)
+                .padding()
+                
+            }
+            .navigationTitle("Journal")
+            .navigationBarTitleDisplayMode(.inline)
+            
         }
-        .padding()
+        
     }
 }
 
