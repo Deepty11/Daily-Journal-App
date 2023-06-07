50746fdfbb068d61885251b488b3af23ca252fee
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-06-07 09:31:55 +0600

    Initial Commit

---
+++ DJournal/DJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJournalApp.swift
+//  DJournal
+//
+//  Created by Rehnuma Reza Deepty on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
